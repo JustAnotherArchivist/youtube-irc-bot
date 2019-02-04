82976b299566137d7c0843523b6b64031e7b279e
Video Archivist <video-archivist@users.noreply.github.com>
2019-02-04 21:37:47 +0000

    !status: show nicely formatted durations

--- src/message.rs
+++ src/message.rs
@@ -151,7 +151,28 @@ fn shorthand_duration(start_time: u64) -> String {
         .expect("Time went backwards");
     let time_now = since_the_epoch.as_secs();
     let duration = time_now - start_time;
-    return format!("{}", duration);
+    duration_to_dhm(duration)
+}
+
+/// seconds -> e.g. 2d4h1m
+fn duration_to_dhm(secs: u64) -> String {
+    match secs {
+        secs if secs < 3600  => {
+            let minutes = secs / 60;
+            format!("{}m", minutes)
+        },
+        secs if secs < 86400 => {
+            let hours = secs / 3600;
+            let minutes = (secs - (hours * 3600)) / 60;
+            format!("{}h{}m", hours, minutes)
+        },
+        secs => {
+            let days  = secs / 86400;
+            let hours = (secs - (days * 86400)) / 3600;
+            let minutes = (secs - (days * 86400) - (hours * 3600)) / 60;
+            format!("{}d{}h{}m", days, hours, minutes)
+        },
+    }
 }
 
 fn get_downloader_sessions() -> Result<Vec<DownloaderSession>, Box<error::Error>> {
