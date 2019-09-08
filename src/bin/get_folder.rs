9e58d284594e06062669f61267623e49f548177c
Video Archivist <video-archivist@users.noreply.github.com>
2019-09-08 20:38:45 +0000

    get_folder: support /watch URLs

--- src/bin/get_folder.rs
+++ src/bin/get_folder.rs
@@ -2,12 +2,14 @@ extern crate youtube_irc_bot;
 
 use std::io::{self, BufRead};
 use youtube_irc_bot::message::get_folder;
+use youtube_irc_bot::http::get_youtube_channel;
 
 fn main() {
     let stdin = io::stdin();
     for line in stdin.lock().lines() {
-        let line = line.unwrap();
-        let out = match get_folder(&line) {
+        let url = line.unwrap();
+        let channel_url = format!("https://www.youtube.com/channel/{}", get_youtube_channel(&url).unwrap());
+        let out = match get_folder(&channel_url) {
             Ok(url) => url,
             Err(err) => format!("# {:?}", err)
         };
