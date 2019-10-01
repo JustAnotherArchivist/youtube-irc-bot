17ab92522d6aa8033dda6b796dad83eece07d55b
Video Archivist <video-archivist@users.noreply.github.com>
2019-10-01 08:26:12 +0000

    Remove unused extern crate

--- src/lib.rs
+++ src/lib.rs
@@ -1,18 +1,7 @@
-extern crate irc;
 #[macro_use]
 extern crate serde_derive;
-extern crate itertools;
-extern crate regex;
 #[macro_use]
 extern crate lazy_static;
-extern crate htmlescape;
-extern crate time;
-extern crate mime;
-extern crate humansize;
-extern crate unicode_segmentation;
-extern crate toml;
-extern crate directories;
-extern crate url;
 
 pub mod config;
 pub mod message;
